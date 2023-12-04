64303c400e2b67013166ebeb478f2cc001d0e642
Pavlo <dikijpavlo84@gmail.com>
2023-12-04 17:11:42 +0200

    Add Hello Word 2.0

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 
 function App() {
   const surname = 'Дикий';
-  const firstName = 'Павлоплрдлдлждл';
-  const group = '2000001';
-  <p>Hello Word!!!</p>
+  const firstName = 'Павло';
+  const group = '203';
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <header className="App-header">
         <h1>{`${surname} ${firstName} ${group} навчальна група`}</h1>
+        <p>Hello Word!!!</p>
       </header>
       </header>
     </div>
